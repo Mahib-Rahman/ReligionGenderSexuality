9697fdc43ac092846453fda834f239f695fea4c3
Mahib Rahman <mahib.7009@gmail.com>
2022-03-23 10:05:56 -0400

    initial changes

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return(
+        <header className="header">
+            <div className="header-title">Religion Gender and Sexuality</div>
+            <div className="header-projectName">Mahib Rahman - Project</div>
+        </header>
+    );
+}
+
+export default Header;
